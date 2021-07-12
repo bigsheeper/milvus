1704279f5748f43b30a3cb88cb2367c0dd97c465
bigsheeper <yihao.dai@zilliz.com>
2021-07-12 10:52:36 +0800

    change start script
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -15,8 +15,36 @@ nohup ./bin/milvus run proxy > /tmp/proxy.log 2>&1 &
 echo "starting querycoord"
 nohup ./bin/milvus run querycoord > /tmp/querycoord.log 2>&1 &
 
-echo "starting querynode"
-nohup ./bin/milvus run querynode > /tmp/querynode.log 2>&1 &
+echo "starting querynode1"
+nohup ./bin/milvus run querynode --alias querynode:1 > /tmp/querynode1.log 2>&1 &
+
+echo "starting querynode2"
+nohup ./bin/milvus run querynode --alias querynode:2 > /tmp/querynode2.log 2>&1 &
+
+echo "starting querynode3"
+nohup ./bin/milvus run querynode --alias querynode:3 > /tmp/querynode3.log 2>&1 &
+
+echo "starting querynode4"
+nohup ./bin/milvus run querynode --alias querynode:4 > /tmp/querynode4.log 2>&1 &
+
+
+
+
+echo "starting querynode5"
+nohup ./bin/milvus run querynode --alias querynode:5 > /tmp/querynode5.log 2>&1 &
+
+echo "starting querynode6"
+nohup ./bin/milvus run querynode --alias querynode:6 > /tmp/querynode6.log 2>&1 &
+
+echo "starting querynode7"
+nohup ./bin/milvus run querynode --alias querynode:7 > /tmp/querynode7.log 2>&1 &
+
+echo "starting querynode8"
+nohup ./bin/milvus run querynode --alias querynode:8 > /tmp/querynode8.log 2>&1 &
+
+
+
+
 
 echo "starting indexcoord"
 nohup ./bin/milvus run indexcoord > /tmp/indexcoord.log 2>&1 &

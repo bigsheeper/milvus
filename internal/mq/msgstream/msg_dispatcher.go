0aa311004ac634dff4433a5b5a90228a075c5d2b
bigsheeper <yihao.dai@zilliz.com>
2022-09-02 16:39:26 +0800

    update test
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/mq/msgstream/msg_dispatcher.go
+++ internal/mq/msgstream/msg_dispatcher.go
@@ -16,184 +16,183 @@
 
 package msgstream
 
-//
-//import (
-//	"context"
-//	"fmt"
-//	"sync"
-//	"time"
-//
-//	"go.uber.org/atomic"
-//	"go.uber.org/zap"
-//
-//	"github.com/milvus-io/milvus/internal/log"
-//	"github.com/milvus-io/milvus/internal/proto/internalpb"
-//)
-//
-//const (
-//	chaseTimeout        = 20 * time.Second
-//	consumerChannelSize = 65536
-//)
-//
-//type chaseStrategy int32
-//
-//const (
-//	block chaseStrategy = iota
-//	exception
-//)
-//
-//type (
-//	pchannel = string
-//	vchannel = string
-//)
-//
-//type DispatchFunc func(msgPack *MsgPack) map[vchannel]*MsgPack
-//
-//type TtMsgDispatcher struct {
-//	nodeID       UniqueID
-//	roleName     string
-//	pchannelName pchannel
-//
-//	consumersMu sync.Mutex
-//	consumers   map[vchannel]chan *MsgPack
-//
-//	currentTick  atomic.Uint64
-//	dispatchFunc DispatchFunc
-//	stream       MsgStream
-//	factory      Factory
-//
-//	closeOnce sync.Once
-//	closeChan chan struct{}
-//}
-//
-//func NewTtMsgDispatcher(nodeID UniqueID, roleName string, pchannelName pchannel, dispatchFunc DispatchFunc, factory Factory) *TtMsgDispatcher {
-//	return &TtMsgDispatcher{
-//		nodeID:       nodeID,
-//		roleName:     roleName,
-//		pchannelName: pchannelName,
-//		consumers:    make(map[vchannel]chan *MsgPack),
-//		dispatchFunc: dispatchFunc,
-//		factory:      factory,
-//		closeChan:    make(chan struct{}),
-//	}
-//}
-//
-//func (m *TtMsgDispatcher) Run() error {
-//	var err error
-//	m.stream, err = m.factory.NewTtMsgStream(context.Background())
-//	if err != nil {
-//		return err
-//	}
-//	subName := fmt.Sprintf("%s-%s-%d", m.roleName, m.pchannelName, m.nodeID)
-//	m.stream.AsConsumer([]pchannel{m.pchannelName}, subName)
-//	defer m.stream.Close()
-//	for {
-//		select {
-//		case <-m.closeChan:
-//			log.Info("close TtMsgDispatcher", zap.String("role", m.roleName), zap.String("pchannel", m.pchannelName))
-//			return nil
-//		case pack, ok := <-m.stream.Chan():
-//			if !ok {
-//				return fmt.Errorf("stream closed, role = %s, pchannel = %s", m.roleName, m.pchannelName)
-//			}
-//			dispatchResults := m.dispatchFunc(pack)
-//			m.consumersMu.Lock()
-//			for k, v := range dispatchResults {
-//				if _, ok = m.consumers[k]; ok {
-//					// TODO: select?
-//					m.consumers[k] <- v
-//				}
-//			}
-//			m.consumersMu.Unlock()
-//			m.currentTick.Store(pack.EndTs)
-//		}
-//	}
-//}
-//
-//func (m *TtMsgDispatcher) Close() {
-//	m.closeOnce.Do(func() {
-//		close(m.closeChan)
-//	})
-//}
-//
-//// chaseToCurrent would consume messages from given position to current consumed position.
-//func (m *TtMsgDispatcher) chaseToCurrent(ctx context.Context, vchannel vchannel, position *MsgPosition) error {
-//	readStream, err := m.factory.NewTtMsgStream(ctx)
-//	if err != nil {
-//		return err
-//	}
-//	defer readStream.Close()
-//	subName := fmt.Sprintf("%s-%d-%s", m.roleName, m.nodeID, vchannel)
-//	readStream.AsConsumer([]string{position.GetChannelName()}, subName)
-//
-//	err = readStream.Seek([]*internalpb.MsgPosition{position})
-//	if err != nil {
-//		return err
-//	}
-//	hasMore := true
-//	for hasMore {
-//		select {
-//		case <-ctx.Done():
-//			return fmt.Errorf("context done, role = %s, vchannel = %s", m.roleName, vchannel)
-//		case msgPack, ok := <-readStream.Chan():
-//			if !ok {
-//				return fmt.Errorf("readStream chan has been closed, role = %s, vchannel = %s", m.roleName, vchannel)
-//			}
-//			if msgPack == nil {
-//				continue
-//			}
-//
-//			dispatchResults := m.dispatchFunc(msgPack)
-//			m.consumersMu.Lock()
-//			for k, v := range dispatchResults {
-//				if _, ok = m.consumers[k]; ok {
-//					// TODO: select?
-//					m.consumers[k] <- v
-//				}
-//			}
-//			m.consumersMu.Unlock()
-//
-//			if msgPack.EndTs >= m.currentTick.Load() {
-//				hasMore = false
-//			}
-//		}
-//	}
-//	return nil
-//}
-//
-//func (m *TtMsgDispatcher) addConsumer(vchannel vchannel) error {
-//	m.consumersMu.Lock()
-//	defer m.consumersMu.Unlock()
-//	if _, ok := m.consumers[vchannel]; ok {
-//		return fmt.Errorf("addConsumer failed, vchannel already existed, vchannel = %s", vchannel)
-//	}
-//	m.consumers[vchannel] = make(chan *MsgPack, consumerChannelSize)
-//	return nil
-//}
-//
-//func (m *TtMsgDispatcher) Register(vchannel vchannel, position *MsgPosition) (<-chan *MsgPack, error) {
-//	ctx, cancel := context.WithTimeout(context.Background(), chaseTimeout)
-//	defer cancel()
-//
-//	err := m.addConsumer(vchannel)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	if position.GetTimestamp() >= m.currentTick.Load() {
-//		log.Info("position is latter than current, no need to chase",
-//			zap.String("role", m.roleName), zap.String("vchannel", vchannel))
-//		m.consumersMu.Lock()
-//		defer m.consumersMu.Unlock()
-//		return m.consumers[vchannel], nil
-//	}
-//
-//	err = m.chaseToCurrent(ctx, vchannel, position)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	m.consumersMu.Lock()
-//	defer m.consumersMu.Unlock()
-//	return m.consumers[vchannel], nil
-//}
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.uber.org/atomic"
+	"go.uber.org/zap"
+
+	"github.com/milvus-io/milvus/internal/log"
+	"github.com/milvus-io/milvus/internal/proto/internalpb"
+)
+
+const (
+	chaseTimeout        = 20 * time.Second
+	consumerChannelSize = 65536
+)
+
+type chaseStrategy int32
+
+const (
+	block chaseStrategy = iota
+	exception
+)
+
+type (
+	pchannel = string
+	vchannel = string
+)
+
+type DispatchFunc func(msgPack *MsgPack) map[vchannel]*MsgPack
+
+type TtMsgDispatcher struct {
+	nodeID       UniqueID
+	roleName     string
+	pchannelName pchannel
+
+	consumersMu sync.Mutex
+	consumers   map[vchannel]chan *MsgPack
+
+	currentTick  atomic.Uint64
+	dispatchFunc DispatchFunc
+	stream       MsgStream
+	factory      Factory
+
+	closeOnce sync.Once
+	closeChan chan struct{}
+}
+
+func NewTtMsgDispatcher(nodeID UniqueID, roleName string, pchannelName pchannel, dispatchFunc DispatchFunc, factory Factory) *TtMsgDispatcher {
+	return &TtMsgDispatcher{
+		nodeID:       nodeID,
+		roleName:     roleName,
+		pchannelName: pchannelName,
+		consumers:    make(map[vchannel]chan *MsgPack),
+		dispatchFunc: dispatchFunc,
+		factory:      factory,
+		closeChan:    make(chan struct{}),
+	}
+}
+
+func (m *TtMsgDispatcher) Run() error {
+	var err error
+	m.stream, err = m.factory.NewTtMsgStream(context.Background())
+	if err != nil {
+		return err
+	}
+	subName := fmt.Sprintf("%s-%s-%d", m.roleName, m.pchannelName, m.nodeID)
+	m.stream.AsConsumer([]pchannel{m.pchannelName}, subName)
+	defer m.stream.Close()
+	for {
+		select {
+		case <-m.closeChan:
+			log.Info("close TtMsgDispatcher", zap.String("role", m.roleName), zap.String("pchannel", m.pchannelName))
+			return nil
+		case pack, ok := <-m.stream.Chan():
+			if !ok {
+				return fmt.Errorf("stream closed, role = %s, pchannel = %s", m.roleName, m.pchannelName)
+			}
+			dispatchResults := m.dispatchFunc(pack)
+			m.consumersMu.Lock()
+			for k, v := range dispatchResults {
+				if _, ok = m.consumers[k]; ok {
+					// TODO: select?
+					m.consumers[k] <- v
+				}
+			}
+			m.consumersMu.Unlock()
+			m.currentTick.Store(pack.EndTs)
+		}
+	}
+}
+
+func (m *TtMsgDispatcher) Close() {
+	m.closeOnce.Do(func() {
+		close(m.closeChan)
+	})
+}
+
+// chaseToCurrent would consume messages from given position to current consumed position.
+func (m *TtMsgDispatcher) chaseToCurrent(ctx context.Context, vchannel vchannel, position *MsgPosition) error {
+	readStream, err := m.factory.NewTtMsgStream(ctx)
+	if err != nil {
+		return err
+	}
+	defer readStream.Close()
+	subName := fmt.Sprintf("%s-%d-%s", m.roleName, m.nodeID, vchannel)
+	readStream.AsConsumer([]string{position.GetChannelName()}, subName)
+
+	err = readStream.Seek([]*internalpb.MsgPosition{position})
+	if err != nil {
+		return err
+	}
+	hasMore := true
+	for hasMore {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context done, role = %s, vchannel = %s", m.roleName, vchannel)
+		case msgPack, ok := <-readStream.Chan():
+			if !ok {
+				return fmt.Errorf("readStream chan has been closed, role = %s, vchannel = %s", m.roleName, vchannel)
+			}
+			if msgPack == nil {
+				continue
+			}
+
+			dispatchResults := m.dispatchFunc(msgPack)
+			m.consumersMu.Lock()
+			for k, v := range dispatchResults {
+				if _, ok = m.consumers[k]; ok {
+					// TODO: select?
+					m.consumers[k] <- v
+				}
+			}
+			m.consumersMu.Unlock()
+
+			if msgPack.EndTs >= m.currentTick.Load() {
+				hasMore = false
+			}
+		}
+	}
+	return nil
+}
+
+func (m *TtMsgDispatcher) addConsumer(vchannel vchannel) error {
+	m.consumersMu.Lock()
+	defer m.consumersMu.Unlock()
+	if _, ok := m.consumers[vchannel]; ok {
+		return fmt.Errorf("addConsumer failed, vchannel already existed, vchannel = %s", vchannel)
+	}
+	m.consumers[vchannel] = make(chan *MsgPack, consumerChannelSize)
+	return nil
+}
+
+func (m *TtMsgDispatcher) Register(vchannel vchannel, position *MsgPosition) (<-chan *MsgPack, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), chaseTimeout)
+	defer cancel()
+
+	err := m.addConsumer(vchannel)
+	if err != nil {
+		return nil, err
+	}
+
+	if position.GetTimestamp() >= m.currentTick.Load() {
+		log.Info("position is latter than current, no need to chase",
+			zap.String("role", m.roleName), zap.String("vchannel", vchannel))
+		m.consumersMu.Lock()
+		defer m.consumersMu.Unlock()
+		return m.consumers[vchannel], nil
+	}
+
+	err = m.chaseToCurrent(ctx, vchannel, position)
+	if err != nil {
+		return nil, err
+	}
+
+	m.consumersMu.Lock()
+	defer m.consumersMu.Unlock()
+	return m.consumers[vchannel], nil
+}
--- internal/mq/msgstream/msg_dispatcher_test.go
+++ internal/mq/msgstream/msg_dispatcher_test.go
@@ -16,150 +16,231 @@
 
 package msgstream
 
-//
-//import (
-//	"context"
-//	"github.com/milvus-io/milvus/internal/proto/commonpb"
-//	"github.com/milvus-io/milvus/internal/proto/internalpb"
-//	"github.com/milvus-io/milvus/internal/util/dependency"
-//	"github.com/stretchr/testify/assert"
-//	"math/rand"
-//	"strconv"
-//	"testing"
-//	"time"
-//)
-//
-//const (
-//	producePackNum = 10
-//	msgNumEachPack = 10
-//)
-//
-//var collectionIDList = []UniqueID{0, 1, 2, 3, 4, 5}
-//var msgTypeList = []commonpb.MsgType{
-//	commonpb.MsgType_Insert,
-//	commonpb.MsgType_Delete,
-//	commonpb.MsgType_TimeTick,
-//	commonpb.MsgType_DropCollection,
-//}
-//
-//func mockDispatcherFunc(msgPack *MsgPack) map[vchannel]*MsgPack {
-//	packs := make(map[vchannel]*MsgPack)
-//	getVChannelName := func(collectionID UniqueID) string {
-//		return strconv.FormatInt(collectionID, 10)
-//	}
-//	initPack := func(beginTs, endTs Timestamp, vchannel vchannel) {
-//		packs[vchannel] = &MsgPack{
-//			BeginTs:        beginTs,
-//			EndTs:          endTs,
-//			Msgs:           make([]TsMsg, 0),
-//			StartPositions: make([]*MsgPosition, 0),
-//			EndPositions:   make([]*MsgPosition, 0),
-//		}
-//	}
-//	for i := range msgPack.Msgs {
-//		switch msgPack.Msgs[i].Type() {
-//		case commonpb.MsgType_Insert:
-//			iMsg := msgPack.Msgs[i].(*InsertMsg)
-//			vchannel := getVChannelName(iMsg.CollectionID)
-//			if _, ok := packs[vchannel]; !ok {
-//				initPack(msgPack.BeginTs, msgPack.EndTs, vchannel)
-//			}
-//			packs[vchannel].Msgs = append(packs[vchannel].Msgs, msgPack.Msgs[i])
-//			packs[vchannel].StartPositions = append(packs[vchannel].StartPositions, msgPack.StartPositions[i])
-//			packs[vchannel].EndPositions = append(packs[vchannel].EndPositions, msgPack.EndPositions[i])
-//		}
-//	}
-//	return packs
-//}
-//
-//func genFactory() dependency.Factory {
-//	factory := dependency.NewDefaultFactory(true)
-//	return factory
-//}
-//
-//func genTsMsg(collectionID UniqueID, msgType commonpb.MsgType, timestamp Timestamp) TsMsg {
-//	base := &commonpb.MsgBase{
-//		MsgType:   msgType,
-//		Timestamp: timestamp,
-//	}
-//	switch msgType {
-//	case commonpb.MsgType_Insert:
-//		return &InsertMsg{
-//			InsertRequest: internalpb.InsertRequest{
-//				Base:         base,
-//				CollectionID: collectionID,
-//			},
-//		}
-//	case commonpb.MsgType_Delete:
-//		return &DeleteMsg{
-//			DeleteRequest: internalpb.DeleteRequest{
-//				Base:         base,
-//				CollectionID: collectionID,
-//			},
-//		}
-//	case commonpb.MsgType_TimeTick:
-//		return &TimeTickMsg{
-//			TimeTickMsg: internalpb.TimeTickMsg{
-//				Base: base,
-//			},
-//		}
-//	case commonpb.MsgType_DropCollection:
-//		return &DropCollectionMsg{
-//			DropCollectionRequest: internalpb.DropCollectionRequest{
-//				Base:         base,
-//				CollectionID: collectionID,
-//			},
-//		}
-//	}
-//	return nil
-//}
-//
-//func genTimestampByIndex(i int) Timestamp {
-//	return Timestamp((i + 1) * 100)
-//}
-//
-//func produceMsg(pchannel pchannel) (error) {
-//	factory := genFactory()
-//	stream, err := factory.NewMsgStream(context.Background())
-//	if err != nil {
-//		return err
-//	}
-//	stream.AsProducer([]string{pchannel})
-//	for i := 0; i < producePackNum; i++ {
-//		beginTs := genTimestampByIndex(i)
-//		endTs := genTimestampByIndex(i + 1)
-//		pack := &MsgPack{
-//			BeginTs: beginTs,
-//			EndTs:   endTs,
-//			Msgs:    make([]TsMsg, 0),
-//		}
-//		for j := 0; j < msgNumEachPack; j++ {
-//			collectionID := collectionIDList[rand.Int()%len(collectionIDList)]
-//			msgType := msgTypeList[rand.Int()%len(msgTypeList)]
-//			timestamp := beginTs + rand.Uint64()%(endTs-beginTs) // beginTs ~ endTs
-//			pack.Msgs = append(pack.Msgs, genTsMsg(collectionID, msgType, timestamp))
-//		}
-//		err = stream.Produce(pack)
-//		if err != nil {
-//			return err
-//		}
-//	}
-//	return nil
-//}
-//
-//func TestTtMsgDispatcher(t *testing.T) {
-//	t.Run("test TtMsgDispatcher", func(t *testing.T) {
-//		pchannel := "testTtMsgDispatcher-pchannel-0"
-//		dispatcher := NewTtMsgDispatcher(0, "role", pchannel, mockDispatcherFunc, genFactory())
-//		go func() {
-//			err := dispatcher.Run()
-//			assert.NoError(t, err)
-//		}()
-//
-//		time.Sleep(100 * time.Millisecond)
-//		err := produceMsg(pchannel)
-//		assert.NoError(t, err)
-//
-//		//dispatcher.Register()
-//	})
-//}
+import (
+	"context"
+	"fmt"
+	"github.com/milvus-io/milvus/internal/proto/commonpb"
+	"github.com/milvus-io/milvus/internal/proto/internalpb"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
+
+const (
+	producePackNum = 10
+	msgNumEachPack = 10
+)
+
+var collectionIDList = []UniqueID{0, 1, 2, 3, 4, 5}
+
+func collectionID2VChannelName(collectionID UniqueID) string {
+	return strconv.FormatInt(collectionID, 10)
+}
+
+func vchannelName2collectionID(vchannel vchannel) UniqueID {
+	id, err := strconv.ParseInt(vchannel, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return id
+}
+
+func mockDispatcherFunc(msgPack *MsgPack) map[vchannel]*MsgPack {
+	packs := make(map[vchannel]*MsgPack)
+	initPack := func(beginTs, endTs Timestamp, vchannel vchannel) {
+		packs[vchannel] = &MsgPack{
+			BeginTs: beginTs,
+			EndTs:   endTs,
+			Msgs:    make([]TsMsg, 0),
+			//StartPositions: make([]*MsgPosition, 0),
+			//EndPositions:   make([]*MsgPosition, 0),
+		}
+	}
+	for i := range msgPack.Msgs {
+		switch msgPack.Msgs[i].Type() {
+		case commonpb.MsgType_Insert:
+			iMsg := msgPack.Msgs[i].(*InsertMsg)
+			vchannel := collectionID2VChannelName(iMsg.CollectionID)
+			if _, ok := packs[vchannel]; !ok {
+				initPack(msgPack.BeginTs, msgPack.EndTs, vchannel)
+			}
+			packs[vchannel].Msgs = append(packs[vchannel].Msgs, msgPack.Msgs[i])
+			//packs[vchannel].StartPositions = append(packs[vchannel].StartPositions, msgPack.StartPositions[i])
+			//packs[vchannel].EndPositions = append(packs[vchannel].EndPositions, msgPack.EndPositions[i])
+		}
+	}
+	return packs
+}
+
+func genFactory(t *testing.T) Factory {
+	defer func() {
+		err := os.Unsetenv("ROCKSMQ_PATH")
+		if err != nil {
+			panic(err)
+		}
+	}()
+	dir := t.TempDir()
+	factory := NewRmsFactory(dir)
+	return factory
+}
+
+func genTsMsg(collectionID UniqueID, msgType commonpb.MsgType, timestamp Timestamp, msgID UniqueID) TsMsg {
+	base := &commonpb.MsgBase{
+		MsgType:   msgType,
+		MsgID:     msgID,
+		Timestamp: timestamp,
+	}
+	switch msgType {
+	case commonpb.MsgType_Insert:
+		return &InsertMsg{
+			BaseMsg: BaseMsg{
+				BeginTimestamp: timestamp,
+				EndTimestamp:   timestamp,
+				HashValues:     []uint32{0},
+			},
+			InsertRequest: internalpb.InsertRequest{
+				Base:         base,
+				CollectionID: collectionID,
+				Timestamps:   []Timestamp{timestamp},
+				RowIDs:       []int64{0},
+				RowData:      []*commonpb.Blob{&commonpb.Blob{}},
+			},
+		}
+	case commonpb.MsgType_Delete:
+		return &DeleteMsg{
+			BaseMsg: BaseMsg{
+				BeginTimestamp: timestamp,
+				EndTimestamp:   timestamp,
+				HashValues:     []uint32{0},
+			},
+			DeleteRequest: internalpb.DeleteRequest{
+				Base:             base,
+				CollectionID:     collectionID,
+				Timestamps:       []Timestamp{timestamp},
+				Int64PrimaryKeys: []int64{0},
+				NumRows:          1,
+			},
+		}
+	case commonpb.MsgType_TimeTick:
+		return &TimeTickMsg{
+			BaseMsg: BaseMsg{
+				BeginTimestamp: timestamp,
+				EndTimestamp:   timestamp,
+				HashValues:     []uint32{0},
+			},
+			TimeTickMsg: internalpb.TimeTickMsg{
+				Base: base,
+			},
+		}
+	case commonpb.MsgType_DropCollection:
+		return &DropCollectionMsg{
+			BaseMsg: BaseMsg{
+				BeginTimestamp: timestamp,
+				EndTimestamp:   timestamp,
+				HashValues:     []uint32{0},
+			},
+			DropCollectionRequest: internalpb.DropCollectionRequest{
+				Base:         base,
+				CollectionID: collectionID,
+			},
+		}
+	}
+	return nil
+}
+
+func genTimestampByIndex(i int) Timestamp {
+	return Timestamp((i + 1) * 10000)
+}
+
+func produceMsg(t *testing.T, pchannel pchannel) map[UniqueID]int {
+	factory := genFactory(t)
+	stream, err := factory.NewMsgStream(context.Background())
+	assert.NoError(t, err)
+	stream.AsProducer([]string{pchannel})
+	msgID := UniqueID(0)
+	counter := make(map[UniqueID]int)
+	for i := 0; i < producePackNum; i++ {
+		beginTs := genTimestampByIndex(i)
+		endTs := genTimestampByIndex(i + 1)
+		pack := &MsgPack{
+			BeginTs: beginTs,
+			EndTs:   endTs,
+			Msgs:    make([]TsMsg, 0),
+		}
+		pack.Msgs = append(pack.Msgs, genTsMsg(0, commonpb.MsgType_TimeTick, beginTs, msgID))
+		msgID++
+		err = stream.Produce(pack)
+		assert.NoError(t, err)
+		fmt.Printf("===== produce timeTick msg, timestamp = %d\n", beginTs)
+
+		pack.Msgs = make([]TsMsg, 0)
+		for j := 0; j < msgNumEachPack; j++ {
+			collectionID := collectionIDList[rand.Int()%len(collectionIDList)]
+			timestamp := beginTs + rand.Uint64()%(endTs-beginTs) // beginTs ~ endTs
+			pack.Msgs = append(pack.Msgs, genTsMsg(collectionID, commonpb.MsgType_Insert, timestamp, msgID))
+			msgID++
+			counter[collectionID]++
+			fmt.Printf("-- collectionID = %d, timestamp = %d\n", collectionID, timestamp)
+		}
+		err = stream.Produce(pack)
+		assert.NoError(t, err)
+	}
+	return counter
+}
+
+func TestTtMsgDispatcher(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	t.Run("test TtMsgDispatcher", func(t *testing.T) {
+		pchannel := "testTtMsgDispatcher-pchannel-0"
+		dispatcher := NewTtMsgDispatcher(0, "role", pchannel, mockDispatcherFunc, genFactory(t))
+		outputs := make(map[vchannel]<-chan *MsgPack)
+		for _, collectionID := range collectionIDList {
+			vchannel := collectionID2VChannelName(collectionID)
+			output, err := dispatcher.Register(vchannel, nil)
+			assert.NoError(t, err)
+			outputs[vchannel] = output
+		}
+
+		go func() {
+			err := dispatcher.Run()
+			assert.NoError(t, err)
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+		expected := produceMsg(t, pchannel)
+		fmt.Println(">>>>>>>>>>>>>>>>>", expected)
+
+		result := make(map[UniqueID]int)
+		time.Sleep(2 * time.Second)
+		wg := &sync.WaitGroup{}
+		for vchannel, output := range outputs {
+			wg.Add(1)
+			vchannel, output := vchannel, output
+			func() {
+				defer wg.Done()
+				for {
+					select {
+					case <-ctx.Done():
+						panic("test timeout")
+					case res := <-output:
+						for range res.Msgs {
+							collectionID := vchannelName2collectionID(vchannel)
+							result[collectionID]++
+							fmt.Println("collectionID ", collectionID, ", ResultNum:", result[collectionID], ", expectedNum:", expected[collectionID])
+							if result[collectionID] == expected[collectionID] {
+								return
+							}
+						}
+					}
+				}
+			}()
+		}
+		wg.Wait()
+	})
+}

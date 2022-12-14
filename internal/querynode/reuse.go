d333de2e5472f612cf55162979f2b4e2ef25ac09
bigsheeper <yihao.dai@zilliz.com>
2022-12-14 20:44:38 +0800

    update
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

---
+++ internal/querynode/reuse.go
@@ -0,0 +1,115 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package querynode
+
+import (
+	"context"
+	"github.com/milvus-io/milvus/internal/mq/msgstream"
+	"github.com/milvus-io/milvus/internal/mq/msgstream/mqwrapper"
+	"github.com/milvus-io/milvus/internal/proto/internalpb"
+	"sync"
+)
+
+// just for test
+
+type dispatcher struct {
+	mu sync.Mutex
+
+	pchannel  string
+	vchannels map[string]chan interface{}
+	curPos    *internalpb.MsgPosition
+
+	factory msgstream.Factory
+}
+
+func (d *dispatcher) start(ctx context.Context, position *internalpb.MsgPosition) error {
+	stream, err := d.factory.NewTtMsgStream(ctx)
+	if err != nil {
+		return err
+	}
+	stream.AsConsumer([]string{d.pchannel}, "aaa", mqwrapper.SubscriptionPositionUnknown)
+	err = stream.Seek([]*internalpb.MsgPosition{position})
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case pack := <-stream.Chan(): // TODO: check ok
+			d.mu.Lock()
+			d.curPos = pack.EndPositions[0]
+			for _, msg := range pack.Msgs {
+				msg.ID() // TODO: msg.VChannel/collection
+				d.vchannels["vchannel-x"] <- msg
+			}
+			d.mu.Unlock()
+		}
+	}
+}
+
+func (d *dispatcher) register(ctx context.Context, vchannel string, position *internalpb.MsgPosition, input chan interface{}) error {
+	d.mu.Lock()
+	if position.GetTimestamp() > d.curPos.GetTimestamp() {
+		d.vchannels[vchannel] = input
+		d.mu.Unlock()
+		return nil
+	}
+
+	boundedBuffer := make(chan interface{}, 1024)
+	d.mu.Lock()
+	d.vchannels[vchannel] = boundedBuffer
+	d.mu.Unlock()
+
+	// pull back
+	stream, err := d.factory.NewTtMsgStream(ctx)
+	if err != nil {
+		return err // TODO: roll back
+	}
+	stream.AsConsumer([]string{d.pchannel}, "aaa", mqwrapper.SubscriptionPositionUnknown)
+	err = stream.Seek([]*internalpb.MsgPosition{position})
+	if err != nil {
+		return err
+	}
+	hasMore := true
+	for hasMore {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case pack := <-stream.Chan():
+			d.mu.Lock()
+			if pack.EndPositions[0].GetTimestamp() >= d.curPos.GetTimestamp() {
+				d.mu.Unlock()
+				hasMore = false
+				break
+			}
+			d.mu.Unlock()
+			input <- pack // TODO: check
+		}
+	}
+
+	for pack := range boundedBuffer {
+		input <- pack
+	}
+
+	d.mu.Lock()
+	delete(d.vchannels, vchannel)
+	d.vchannels[vchannel] = input
+	d.mu.Unlock()
+	return nil
+}

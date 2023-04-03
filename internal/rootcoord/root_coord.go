0013af637dd1df55fda08ff3d19301d97932f8a3
bigsheeper <yihao.dai@zilliz.com>
2023-04-03 19:57:37 +0800

    Add update ts log
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/rootcoord/root_coord.go
+++ internal/rootcoord/root_coord.go
@@ -1423,6 +1423,11 @@ func (c *Core) UpdateChannelTimeTick(ctx context.Context, in *internalpb.Channel
 		msgTypeName := commonpb.MsgType_name[int32(in.Base.GetMsgType())]
 		return failStatus(commonpb.ErrorCode_UnexpectedError, "invalid message type "+msgTypeName), nil
 	}
+	log.RatedInfo(60, "dyh, RootCoord UpdateChannelTimeTick",
+		zap.Uint64s("timestamps", in.GetTimestamps()),
+		zap.Strings("channels", in.GetChannelNames()),
+		zap.Uint64("default_ts", in.GetDefaultTimestamp()),
+	)
 	err := c.chanTimeTick.updateTimeTick(in, "gRPC")
 	if err != nil {
 		log.Warn("failed to updateTimeTick", zap.String("role", typeutil.RootCoordRole),

62bca062b59f461e4aadc0e6222fc6c09247f834
Cai Zhang <cai.zhang@zilliz.com>
2024-07-12 12:29:01 +0800

    Remove sement writer to storage package
    Signed-off-by: Cai Zhang <cai.zhang@zilliz.com>

--- pkg/util/paramtable/component_param.go
+++ pkg/util/paramtable/component_param.go
@@ -3174,6 +3174,8 @@ type dataCoordConfig struct {
 	ClusteringCompactionSlotUsage ParamItem `refreshable:"true"`
 	MixCompactionSlotUsage        ParamItem `refreshable:"true"`
 	L0DeleteCompactionSlotUsage   ParamItem `refreshable:"true"`
+
+	EnableStatsTask ParamItem `refreshable:"true"`
 }
 
 func (p *dataCoordConfig) init(base *BaseTable) {
@@ -3977,6 +3979,16 @@ During compaction, the size of segment # of rows is able to exceed segment max #
 		Export:       true,
 	}
 	p.L0DeleteCompactionSlotUsage.Init(base.mgr)
+
+	p.EnableStatsTask = ParamItem{
+		Key:          "dataCoord.statsTask.enable",
+		Version:      "2.5.0",
+		Doc:          "enable stats task",
+		DefaultValue: "false",
+		PanicIfEmpty: false,
+		Export:       false,
+	}
+	p.EnableStatsTask.Init(base.mgr)
 }
 
 // /////////////////////////////////////////////////////////////////////////////

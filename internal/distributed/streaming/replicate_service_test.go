f56a07716f544ea367a8f48d962d0b8784750590
bigsheeper <yihao.dai@zilliz.com>
2026-02-15 21:47:37 +0800

    enhance: allow secondary cluster to use local replica config for replicated AlterLoadConfig
    When a replicated AlterLoadConfigMessage is received on the secondary cluster,
    use the local cluster-level replica config (ClusterLevelLoadReplicaNumber /
    ClusterLevelLoadResourceGroups) instead of the primary's replica config.
    
    - Add use_local_replica_config field to AlterLoadConfigMessageHeader proto
    - Set the flag in ReplicateService.overwriteReplicateMessage for AlterLoadConfig
    - In LoadCollectionJob.Execute, read local config when the flag is set
    - Use generateReplicas for idempotent replica generation on WAL replay
    - Default to 1 replica in __default_resource_group when local config not set
    
    Signed-off-by: bigsheeper <yihao.dai@zilliz.com>

--- internal/distributed/streaming/replicate_service_test.go
+++ internal/distributed/streaming/replicate_service_test.go
@@ -17,12 +17,10 @@ import (
 	"github.com/milvus-io/milvus/internal/mocks/streamingcoord/mock_client"
 	"github.com/milvus-io/milvus/internal/mocks/streamingnode/client/handler/mock_producer"
 	"github.com/milvus-io/milvus/internal/mocks/streamingnode/client/mock_handler"
-	"github.com/milvus-io/milvus/internal/util/streamingutil/status"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/util/message"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/util/types"
 	pulsar2 "github.com/milvus-io/milvus/pkg/v2/streaming/walimpls/impls/pulsar"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/walimpls/impls/walimplstest"
-	"github.com/milvus-io/milvus/pkg/v2/util/paramtable"
 	"github.com/milvus-io/milvus/pkg/v2/util/replicateutil"
 	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
 )
